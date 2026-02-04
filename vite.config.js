338f40d1d22aa55d6d8c6058846223c5b67c52f6
chromazen <chromazen.zen@gmail.com>
2026-02-04 21:27:58 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/chroma3d/",
   plugins: [react()],
-  publicDir: "public", // 👈 FORCE copy public
+  base: "/chroma3d/",
 });
