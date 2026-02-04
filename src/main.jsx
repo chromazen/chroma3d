17b3460837cef43522ca330b7c6616c9a4e499a1
chromazen <chromazen.zen@gmail.com>
2026-02-04 20:25:03 +0530

    Update main.jsx for Deployement with basename

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/chroma3d">
       <App />
     </BrowserRouter>
   </React.StrictMode>
